73ac68a96ad1170a68482a9ef3eaa5b321491c7a
yutaka.miyazaki <mono.12dev@gmail.com>
2021-11-11 18:26:15 +0900

    add new submission

---
+++ easy/binary-tree-inorder-traversal/index.py
@@ -0,0 +1,18 @@
+from typing import List, Optional
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        result = []
+        if root:
+            result += self.inorderTraversal(root.left)
+            result += [root.val]
+            result += self.inorderTraversal(root.right)
+            
+        return result
